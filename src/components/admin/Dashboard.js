dba50c54b5e5a8d55a3719aac66e3e2192097917
pravesh1731 <706890pk@gmail.com>
2026-08-03 22:55:02 +0530

    deployment error fix3

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from "react";
+import {React, useCallback, useEffect, useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setApplications } from "../../app/slice/mtechSlice";
 
